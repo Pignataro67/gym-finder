aeb472388f63f90a5fb4de4720dae35a056acceb
Pat <pignataro67@gmail.com>
2018-03-23 11:26:06 -0400

    adding file with new and create methods

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  before_action :set_user
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.admin = true if params[:user][:admin] == "true"
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+  end
+end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+end
+
