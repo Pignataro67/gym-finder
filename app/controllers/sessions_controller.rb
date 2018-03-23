98cecb5668a1c726df139199647a6bdf21558763
Pat <pignataro67@gmail.com>
2018-03-23 11:58:56 -0400

    adding file with new and create methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if !!auth
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.email = auth['info']['email']
+        u.user_name = auth['info']['name']
+        u.password = SecureRandom.hex
+      end
+      @user.save
+      session[:user_id] = @user.id
+      render 'gyms/index'
+    else
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:message] = "You have loggied in successfully!"
+        redirect_to root_path
+      else
+        flash[:message] = "Please enter a valid email/password!"
+        render :new
+      end
+    end
+  end
+
+  end
+
+  def destroy
+  end
+end
