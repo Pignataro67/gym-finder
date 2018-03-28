3502b7ee91a5dc067b3a8cd1c1c2740d409bb501
Pat <pignataro67@gmail.com>
2018-03-27 20:15:26 -0400

    adding file with user authen./valid. logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?, :authentication_required, :is_admin?
+  
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+  
+  def authentication_required
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+
+  def is_admin?
+    current_user.admin == true ? true : false if current_user
+  end
 end
