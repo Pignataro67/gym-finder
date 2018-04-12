f22280c605cf14f18915d0c071506af713cc04c1
Pat <pignataro67@gmail.com>
2018-04-12 10:56:49 -0400

    adding file with private params method

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  before_action :set_user
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.admin = true if params[:user][:admin] == "true"
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+  end
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+end
