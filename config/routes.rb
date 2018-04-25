4dc49001465a497f0748d5d6f01845f3262dda6d
Pat <pignataro67@gmail.com>
2018-04-25 19:20:54 -0400

    adding file with github oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'gyms#index'
+
+  resources :gyms, only: [:show, :create, :edit, :update, :destroy]
+
+  resources :reviews
+
+  get 'signup' => 'users#new', as: :signup
+  post 'users' => 'users#create', as: :users
+
+  get '/auth/github/callback' => 'sessions#create'
+
+  get 'login' => 'sessions#new', as: :login
+  post 'sessions' => 'sessions#create', as: :sessions
+
+  get 'logout' => 'sessions#destroy'
+
+  resources :users, only: [:show] do
+    resources :gyms, only: [:index, :create, :show, :new]
+  end
 end
