a81066b63f5b01d89a050ceb0649e394bbd31c2f
Pat <pignataro67@gmail.com>
2018-04-27 16:11:21 -0400

    adding file with minimum validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :gyms, through: :reviews
 
+  has_secure_password
   validates :user_name, :email, presence: true
   validates :email, uniqueness: true
 
-  has_secure_password
+  validates :password_digest, length: { minimum: 7 }, allow_nil: true
 end
