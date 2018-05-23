058b39566be1238b26c21933944f7c018d24595d
Pat <pignataro67@gmail.com>
2018-05-23 19:46:16 -0400

    adding file with a scope method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :gym
+
+  default_scope -> { order(created_at: :desc) }
+
+  scope :recent_reviews, -> { order(created_at: :desc).limit(10) }
 end
