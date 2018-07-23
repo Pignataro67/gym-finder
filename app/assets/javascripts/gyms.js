8bebe4bf7980e4604703ebf2c0d7ee18e00af822
Pat <pignataro67@gmail.com>
2018-07-23 16:39:59 -0400

    adding file Gym constructor function and declaring a new gym instance

--- app/assets/javascripts/gyms.js
+++ app/assets/javascripts/gyms.js
@@ -8,6 +8,18 @@ const bindClickHandlers = () => {
     e.preventDefault();
     $.get('/gyms.json').done((gyms) => {
       $("#app-container").html('')
+      gyms.forEach( function (gym) {
+        let newGym = new Gym(gym)
+      })
     })
   })
+}
+
+function Gym(gym) {
+  this.id = gym.id
+  this.name = gym.name
+  this.location = gym.location
+  this.classes = gym.classes
+  this.reviews = gym.reviews
+  this.users = gym.users
 }
