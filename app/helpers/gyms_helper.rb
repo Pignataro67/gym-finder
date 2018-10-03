53b241ff6f41a6724edd3837962f45257603f826
Pignataro67 <Pignataro67@gmail.com>
2018-10-03 12:28:20 -0700

    adding file with improved functionality

--- app/helpers/gyms_helper.rb
+++ app/helpers/gyms_helper.rb
@@ -1,8 +1,19 @@
-module GymHelper
+module GymsHelper
+
+  def has_reviews(gym)
+    !gym.reviews.empty? ? true : false
+  end
 
   def gym_owner(gym)
     owner = gym.users.where(admin: true).first
     owner == current_user ? true : false
   end
+
+  def add_date_to_review(gym)
+    first_review = gym.reviews.first
+    first_review.date = Date.today
+    first_review.save
+  end 
 end
+
 
